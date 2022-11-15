2a2ec443a8837388d38a52cb4fb11159c9e5f44a
violetomo <violetomo@gmail.com>
2022-11-15 19:32:36 +0600

    add linter command

--- gendiff/modules/gendiff.py
+++ gendiff/modules/gendiff.py
@@ -1,29 +1,29 @@
 import json
-import pathlib
 from pathlib import Path
 
 
 def generate_diff(file1, file2):
-    dir_path = Path.home()
-    filepath1 = next(Path(dir_path).rglob(file1))
-    filepath2 = next(Path(dir_path).rglob(file2))
-    file1_dict = json.load(open(filepath1, encoding='utf-8'))
-    file2_dict = json.load(open(filepath2, encoding='utf-8'))
-    f1_d_sorted = dict(sorted(file1_dict.items()))
-    f2_d_sorted = dict(sorted(file2_dict.items()))
+    def find_and_sort(file):
+        dir_path = Path.home()
+        file_path = next(Path(dir_path).rglob(file))
+        file_dict = json.load(open(file_path, encoding='utf-8'))
+        dict_sorted = dict(sorted(file_dict.items()))
+        return dict_sorted
+    f1_d_sorted = find_and_sort(file1)
+    f2_d_sorted = find_and_sort(file2)
     diff = ''
     diff_dict = {}
     for key in f1_d_sorted:
         if key not in f2_d_sorted:
-            diff_dict['- '+key] = f1_d_sorted[key]
-        elif key in f2_d_sorted and f1_d_sorted[key] == f2_d_sorted[key]:
-            diff_dict['  '+key] = f1_d_sorted[key]
+            diff_dict['- ' + key] = f1_d_sorted[key]
+        elif f1_d_sorted[key] == f2_d_sorted[key]:
+            diff_dict['  ' + key] = f1_d_sorted[key]
         else:
-            diff_dict['- '+key] = f1_d_sorted[key]
-            diff_dict['+ '+key] = f2_d_sorted[key]
+            diff_dict['- ' + key] = f1_d_sorted[key]
+            diff_dict['+ ' + key] = f2_d_sorted[key]
     for key in f2_d_sorted:
         if key not in f1_d_sorted:
-            diff_dict['+ '+key] = f2_d_sorted[key]
+            diff_dict['+ ' + key] = f2_d_sorted[key]
     for key in diff_dict:
         diff += f'\n {key}: {str(diff_dict[key]).lower()}'
     diff = '{' + diff + '\n}'
